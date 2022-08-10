56210498c6e3fe549887bc84e4c7f398f732d0ef
yuya.xyf <yuya.xyf@alibaba-inc.com>
2022-08-10 19:57:38 +0800

    feat: update node-user-service  to v1.2.2

--- packages/mpserverless-node-user-service/dist/umd/index.d.ts
+++ packages/mpserverless-node-user-service/dist/umd/index.d.ts
@@ -5,4 +5,9 @@ export interface GetInfoOptions {
 export declare class FunctionUserService extends BaseService {
     protected appSecret: string;
     getInfo(options?: GetInfoOptions): Promise<any>;
+    proxyHttpClientGetRequest(url: string, query?: object): Promise<any>;
+    proxyHttpClientPostRequest(url: string, text?: string, headers?: object): Promise<any>;
+    proxyHttpClientPostJsonRequest(url: string, json?: object, headers?: object): Promise<any>;
+    proxyHttpClientPostFormDataRequest(url: string, data?: object, headers?: object): Promise<any>;
+    protected proxyHttpClientRequest(url: string, method: 'GET' | 'POST', body?: object | string, headers?: object): Promise<any>;
 }
