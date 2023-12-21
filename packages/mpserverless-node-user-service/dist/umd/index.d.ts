9e9a1cb3e2e7bbb527b240e8955cf8c5a19b933e
yuya.xyf <yuya.xyf@alibaba-inc.com>
2023-12-21 15:17:08 +0800

    feat: update node-user-service  to v1.2.3

--- packages/mpserverless-node-user-service/dist/umd/index.d.ts
+++ packages/mpserverless-node-user-service/dist/umd/index.d.ts
@@ -5,9 +5,15 @@ export interface GetInfoOptions {
 export declare class FunctionUserService extends BaseService {
     protected appSecret: string;
     getInfo(options?: GetInfoOptions): Promise<any>;
-    proxyHttpClientGetRequest(url: string, query?: object): Promise<any>;
+    proxyHttpClientGetRequest(url: string, query?: object, headers?: object): Promise<any>;
     proxyHttpClientPostRequest(url: string, text?: string, headers?: object): Promise<any>;
     proxyHttpClientPostJsonRequest(url: string, json?: object, headers?: object): Promise<any>;
     proxyHttpClientPostFormDataRequest(url: string, data?: object, headers?: object): Promise<any>;
     protected proxyHttpClientRequest(url: string, method: 'GET' | 'POST', body?: object | string, headers?: object): Promise<any>;
+    protected report(options: {
+        FCCost: number;
+        FCRequestCount: number;
+        FCTXTraffic: number;
+    }): Promise<void>;
+    protected redisCli(commands: string[]): Promise<any>;
 }
